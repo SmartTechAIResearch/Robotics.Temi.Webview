e786d34015a318a7eb7038f063e035873c94e1dc
NielsAudoor <29575824+NielsAudoor@users.noreply.github.com>
2022-08-16 15:19:45 +0200

    React  Webview Test

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { io } from "socket.io-client";
+
+const socket = io("http://172.30.248.58:8453");
 
 function App() {
+  const [isConnected, setIsConnected] = useState(socket.connected);
+  const [lastPong, setLastPong] = useState(null);
+
+  useEffect(() => {
+    socket.on("connect", () => {
+      setIsConnected(true);
+    });
+    socket.on("disconnect", () => {
+      setIsConnected(false);
+    });
+    socket.on("testReturn", () => {
+      console.log("Works");
+    });
+  });
+
+  const sendMessage = () => {
+    socket.emit("message", "home base");
+  };
+  const sendBank = () => {
+    socket.emit("message", "bank");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Connected: {"" + isConnected}</p>
+      <button onClick={sendMessage}>Home Base</button>
+      <button onClick={sendBank} className="buttonRight">
+        Bank
+      </button>
     </div>
   );
 }
