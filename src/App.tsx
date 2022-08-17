9324179dc66a51b86dcfa57183dd13f547625ec8
NielsAudoor <29575824+NielsAudoor@users.noreply.github.com>
2022-08-17 13:56:11 +0200

    gitflow-feature-stash: UI-Design

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
 import { io } from "socket.io-client";
-
+import { Wifi, WifiOff } from "@mui/icons-material";
+import { pink, red } from "@mui/material/colors";
 const socket = io("https://mcttemisocket.azurewebsites.net");
 
 function App() {
@@ -22,14 +23,31 @@ function App() {
   const sendBank = () => {
     socket.emit("message", "bank");
   };
+  const sendLocation = (location: string) => {
+    socket.emit("message", location);
+  };
 
   return (
-    <div className="App">
-      <p>Connected: {"" + isConnected}</p>
-      <button onClick={sendMessage}>Home Base</button>
-      <button onClick={sendBank} className="buttonRight">
-        Bank
-      </button>
+    <div>
+      <img src="/mctLogo.jpg" alt="mctLgo" className="lowerleft"></img>
+      {isConnected ? (
+        <Wifi className="topright" sx={{ fontSize: 40 }} />
+      ) : (
+        <WifiOff className="topright" sx={{ fontSize: 40, color: red[500] }} />
+      )}
+      <div className="App">
+        <p>Connected: {"" + isConnected}</p>
+        <button onClick={sendMessage}>Home Base</button>
+        <button onClick={sendBank} className="buttonRight">
+          Bank
+        </button>
+        <button
+          onClick={() => window.location.reload()}
+          className="buttonRight"
+        >
+          reload{" "}
+        </button>
+      </div>
     </div>
   );
 }
