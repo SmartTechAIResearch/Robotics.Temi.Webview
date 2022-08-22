7dc9bf02ff2b01d8b9a6ab673265a02cd3c05d98
LagrouCedric <71886193+LagrouCedric@users.noreply.github.com>
2022-08-22 12:14:15 +0200

    Header changed + added refresh button and stepper

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,15 @@ import { iLocationData } from "../interfaces/interfaces";
 import { io } from "socket.io-client";
 import { Menu, Wifi, WifiOff } from "@mui/icons-material";
 import { red } from "@mui/material/colors";
+import RefreshIcon from '@mui/icons-material/Refresh';
+import stepper, { Step, StepLabel, Stepper, styled } from '@mui/material'
+import StepConnector, { stepConnectorClasses } from '@mui/material/StepConnector';
+import { StepIconProps } from '@mui/material/StepIcon';
+import Stack from '@mui/material/Stack';
+import VideoLabelIcon from '@mui/icons-material/VideoLabel';
+import SettingsIcon from '@mui/icons-material/Settings';
+import GroupAddIcon from '@mui/icons-material/GroupAdd';
+import Check from '@mui/icons-material/Check';
 import {
   Box,
   Button,
@@ -16,6 +25,70 @@ const socket = io("https://mcttemisocket.azurewebsites.net");
 //const socket = io("http://172.30.248.58:8453");
 
 function App() {
+  const QontoStepIconRoot = styled('div')<{ ownerState: { active?: boolean } }>(
+    ({ theme, ownerState }) => ({
+      color: theme.palette.mode === 'dark' ? theme.palette.grey[700] : '#eaeaf0',
+      display: 'flex',
+      height: 22,
+      alignItems: 'center',
+      ...(ownerState.active && {
+        color: '#784af4',
+      }),
+      '& .QontoStepIcon-completedIcon': {
+        color: '#784af4',
+        zIndex: 1,
+        fontSize: 18,
+      },
+      '& .QontoStepIcon-circle': {
+        width: 8,
+        height: 8,
+        borderRadius: '50%',
+        backgroundColor: 'currentColor',
+      },
+    }),
+  );
+  
+  function QontoStepIcon(props: StepIconProps) {
+    const { active, completed, className } = props;
+  
+    return (
+      <QontoStepIconRoot ownerState={{ active }} className={className}>
+        {completed ? (
+          <Check className="QontoStepIcon-completedIcon" />
+        ) : (
+          <div className="QontoStepIcon-circle" />
+        )}
+      </QontoStepIconRoot>
+    );
+  }
+  
+  const QontoConnector = styled(StepConnector)(({ theme }) => {
+    return ({
+      [`&.${stepConnectorClasses.alternativeLabel}`]: {
+        top: 10,
+        left: 'calc(-50% + 16px)',
+        right: 'calc(50% + 16px)',
+      },
+      [`&.${stepConnectorClasses.active}`]: {
+        [`& .${stepConnectorClasses.line}`]: {
+          borderColor: '#784af4',
+        },
+      },
+      [`&.${stepConnectorClasses.completed}`]: {
+        [`& .${stepConnectorClasses.line}`]: {
+          borderColor: '#784af4',
+        },
+      },
+      [`& .${stepConnectorClasses.line}`]: {
+        borderColor: theme.palette.mode === 'dark' ? theme.palette.grey[800] : '#eaeaf0',
+        borderTopWidth: 3,
+        borderRadius: 1,
+      },
+    });
+  });
+  
+
+
   const [isConnected, setIsConnected] = useState(socket.connected);
   const [open, setOpen] = useState(false);
   const [locationData, setLocationData] = useState<Array<iLocationData>>([]);
@@ -114,33 +187,42 @@ function App() {
       ))}
     </Box>
   );
-
+  const steps = ["Reception", "Project-One", "Core", "the end"]
   return (
     <div>
       <Drawer open={open} anchor={"left"} onClose={() => setOpen(false)}>
-        {getList()}
-      </Drawer>
+            {getList()}
+          </Drawer>
       <img src="/mctLogo.jpg" alt="mctLgo" className="lowerleft"></img>
-      <Button onClick={() => setOpen(true)}>
-        <Menu className="topleft" sx={{ fontSize: 40, color: "black" }} />
-      </Button>
-      {isConnected ? (
-        <Wifi className="topright" sx={{ fontSize: 40 }} />
-      ) : (
-        <WifiOff className="topright" sx={{ fontSize: 40, color: red[500] }} />
-      )}
+      
+      
       <div className="App">
-        <p>Connected: {"" + isConnected}</p>
-        <button onClick={sendMessage}>Home Base</button>
-        <button onClick={sendBank} className="buttonRight">
-          Bank
+          <Button id="HamburgerMenuButton" onClick={() => setOpen(true)}>
+            <Menu className="topleft" sx={{ fontSize: 40, color: "black" }} />
+          </Button>
+          <Stack  sx={{ width: '50%' }} spacing={4}>
+          <Stepper id="stepper" alternativeLabel activeStep={1} connector={<QontoConnector />}>
+            {steps.map((label) => (
+              <Step key={label}>
+                <StepLabel StepIconComponent={QontoStepIcon}>{label}</StepLabel>
+              </Step>
+            ))}
+          </Stepper>
+          
+        </Stack>
+        <div>
+        <button id="refreshPage" onClick={() => window.location.reload()}>
+            <RefreshIcon  sx={{ fontSize: 40 }}></RefreshIcon>
         </button>
-        <button
-          onClick={() => window.location.reload()}
-          className="buttonRight"
-        >
-          reload{" "}
-        </button>
+        {isConnected ? (
+            <Wifi className="topright" sx={{ fontSize: 40 }} />
+          ) : (
+            <WifiOff className="topright" sx={{ fontSize: 40, color: red[500] }} />
+          )}
+          
+        </div>
+      
+      
       </div>
     </div>
   );
