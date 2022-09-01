e43b64b75656443acf083a891054af4b947deb77
NielsAudoor <29575824+NielsAudoor@users.noreply.github.com>
2022-09-01 08:57:19 +0200

    Added AdminPage

--- src/index.tsx
+++ src/index.tsx
@@ -3,17 +3,19 @@ import ReactDOM from "react-dom/client";
 import "./css/index.css";
 import App from "./App";
 import reportWebVitals from "./WebVitals/reportWebVitals";
-import Admin from "./Admin";
+import { Admin } from "./Admin";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <>
-    <App />
-    <Admin />
-  </>
-  
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="admin" element={<Admin />} />
+    </Routes>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
